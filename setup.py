8b27df5063e5fc7b433efed19a21faa2267c4cbc
Sajid Ali <30510036+s-sajid-ali@users.noreply.github.com>
2018-11-28 12:05:36 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy as np 
@@ -6,19 +6,19 @@ import numpy as np
 ext_modules=[
         Extension('prop1d',
         sources=['xwp_cython/prop1d.pyx'],
-        extra_compile_args=['-O3','-fopenmp','-mavx'],
+        extra_compile_args=['-O3','-fopenmp','-xcore-avx2','-I/opt/intel/compilers_and_libraries_2018.3.222/linux/compiler/lib/intel64/libimf.a'],
         extra_link_args=['-fopenmp'],
         language='c'),
     
         Extension('prop2d_2loop',
         sources=['xwp_cython/prop2d_2loop.pyx'],
-        extra_compile_args=['-O3','-fopenmp','-mavx'],
+        extra_compile_args=['-O3','-fopenmp','-xcore-avx2','-I/opt/intel/compilers_and_libraries_2018.3.222/linux/compiler/lib/intel64/libimf.a'],
         extra_link_args=['-fopenmp'],
         language='c'),
     
         Extension('prop2d_4loop',
         sources=['xwp_cython/prop2d_4loop.pyx'],
-        extra_compile_args=['-O3','-fopenmp','-mavx'],
+        extra_compile_args=['-O3','-fopenmp','-xcore-avx2','-I/opt/intel/compilers_and_libraries_2018.3.222/linux/compiler/lib/intel64/libimf.a'],
         extra_link_args=['-fopenmp'],
         language='c')
         ]
